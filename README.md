6c35b171eddb461517b243aa64ba5d5f4fc54a86
Jeff Macaluso <samacaluso@vassar.edu>
2016-10-31 11:21:05 -0400

    Initial commit
    Copy of DevSite

---
+++ README.md
@@ -0,0 +1,3 @@
+# DevSite
+
+This is a set of files for a designer to download and begin editing css files for a custom academic design. This would be a good place to put instructions on setting up a new site.
---
+++ assets/config.php
@@ -0,0 +1,82 @@
+<?php
+return 
+	array(
+	
+	// CMS site ID
+	'siteID' => '47896',//'32864',
+	
+	'dev' => 'new',
+
+	// Fonts
+	'typekitID' => '',//'xib8icl',
+	
+	//'useLocalNav' => true,
+		
+	//'phpErrorReporting' => E_ALL,
+	//'debug' => true,
+	
+	
+	/**
+	 * Complete listing of configurations - turn on debug and see $configSettings
+	 *
+	
+	'cssExternalAdditional' => null,
+	'debug' => null,
+	'galleriaTheme' => "azur",
+	'galleriaVersion' => "1.3.5",
+	'hasDropdowns' => false,
+	'isLive' => "0",
+	'isLiveSite' => "0",
+	'jsExternalAdditional' => null,
+	'loadCollegeWebsite' => false,
+	'loadGA' => "0",
+	'loadResponsive' => true,
+	'loadSearch' => true,
+	'loadSpectate' => "0",
+	'loadTabs' => false,
+	'loadTypekit' => true,
+	'newsKeywords' => "Latin American Studies",
+	'notes' => "Verified CMS match",
+	'officeBoxNumber' => "222",
+	'officeEmail' => "latinamstud",
+	'officeFax' => "",
+	'officeLocationBuilding' => "Old Laundry Building",
+	'officeLocationBuildingCode' => "OLB",
+	'officeLocationRoom' => "",
+	'officePhone' => "7668",
+	'officeURL' => "",
+	'phpErrorReporting' => integer 0,
+	'recordForSite' => "a:32:{s:6:"siteID";s:5:"32864";s:13:"siteIDClaimed";s:1:"1"; …",
+	'searchCode' => "001814011871879729902:smqot3ivpoa",
+	'searchPlaceholderText' => null,
+	'siteCode' => "lals",
+	'siteDesignType' => "Academic",
+	'siteDesignedDate' => "2000-00-00",
+	'siteDesigner' => "SiteID claimed 2013-09-05",
+	'siteHealthRating' => "0",
+	'siteID' => "32864",
+	'siteIDClaimed' => "1",
+	'siteLiveURL' => "http://latinamericanstudies.vassar.edu",
+	'siteName' => "Latin American & Latino/a Studies",
+	'siteNameCMS' => "",
+	'siteNameFooter' => "",
+	'siteNameHeader' => "<b class="u-Masthead__siteName--line1">Latin American</b> <b …",
+	<b class="u-Masthead__siteName--line1">Latin American</b> <b class="u-Masthead__siteName--line2">&amp;</b> <b class="u-Masthead__siteName--line3">Latino/a Studies</b>,
+	'siteNameReportDisplayName' => null,
+	'siteNameSubhead' => "",
+	'sitePathCode' => "latinam",
+	'siteReportDisplayName' => "Latin American and Latino/a Studies QD",
+	'siteReportTitle' => "Latin American and Latino/a Studies QD",
+	'siteSkin' => null,
+	'siteStatus' => "Omit",
+	'siteType' => "Academic",
+	'typekitFade' => false,
+	'typekitFadeDuration' => null,
+	'typekitID' => "xib8icl",
+	'typekitSelector' => null,
+	'versionGamut' => "",
+	'versionjQuery' => "1.11.0",
+	'versionjQueryUI' => "1.9.1",
+	
+*/
+);

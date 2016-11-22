961810bb38a56899cf0ec9afa482ac877de0966b
Jeff Macaluso <samacaluso@vassar.edu>
2016-11-22 16:58:34 -0500

    Pre-Thanksgiving styles
    Applied to both Alums and Families

---
+++ assets/plugins/extendedFooter/config.php
@@ -0,0 +1,79 @@
+<?php
+return 
+array(
+    "plugin"=>"extendedFooter",
+    "content"=><<<CONTENT
+    <div class="u-ExtendedFooter">
+        <div class="u-footMasthead"><h1 class="u-footMasthead__siteName"><a class="u-footMasthead__siteName--link" href="/">Alumnae/i</a></h1><h2 class="u-footMasthead__vassar"><a class="icon-back u-footMasthead__vassar--link" href="http://www.vassar.edu"><b class="">Vassar</b></a></h2>
+        </div>
+        <div class="footerExtra">
+            <ul class="SocialAccount__iconList">
+            <li class="SocialAccount__iconContainer"><a class="SocialAccount__iconTarget" href="#"><svg aria-labelledby="fb" role="img" viewBox="0 0 180 180" width="16" height="16"><title id="fb">Facebook</title><path d="M180 180V0H0v180h180z" fill="#CC2249"></path><path d="M124.2 180v-69.7h23.4l3.5-27h-26.8V65.7c0-8 2.2-13.2 13.5-13.2H152V28.3c-2.5-.3-11-1-21-1-20.6 0-35 12.6-35 35.8v20H72.8v27.3h23.5V180h28z" fill="#380C2A"></path><image alt="Facebook" height="16" src="/rayschwartz/hp-responsive/assets/images/icons/icon-16-facebook.png" width="16" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href=""></image></svg></a></li>
+            <li class="SocialAccount__iconContainer"><a class="SocialAccount__iconTarget" href="#"><svg aria-labelledby="tw" role="img" viewBox="0 0 404 404" width="16" height="16"><title id="tw">Twitter</title><path fill="#CC2249" d="M0 0h404v404H0z"></path><path d="M340 115c-10 4.4-21 7.4-32.2 8.8 11.5-7 20.4-18 24.6-31-10.8 6.4-22.8 11-35.6 13.6-10.2-11-24.7-17.7-40.8-17.7-31 0-56 25-56 56 0 4.4.5 8.6 1.5 12.7C155 155 113.8 132.8 86.2 99c-5 8.2-7.6 17.8-7.6 28 0 19.5 10 36.6 25 46.7-9.3-.3-18-2.8-25.5-7v.7c0 27 19.4 49.7 45 54.8-4.7 1.3-9.6 2-14.7 2-3.6 0-7-.4-10.6-1 7.2 22.2 27.8 38.4 52.3 38.8-19 15-43.3 24-69.5 24-4.5 0-9-.3-13.3-.8 24.7 16 54.2 25.2 85.8 25.2 103 0 159-85.3 159-159.2V144c11-8 20.4-17.8 28-29z" fill="#380C2A"></path><image alt="Twitter" height="16" src="/rayschwartz/hp-responsive/assets/images/icons/icon-16-twitter.png" width="16" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href=""></image></svg></a></li>
+            <li class="SocialAccount__iconContainer"><a class="SocialAccount__iconTarget" href="#"><svg aria-labelledby="ig" role="img" viewBox="0 0 500 500" width="16" height="16"><title id="ig">Instagram</title><rect x="42" y="42" fill="#380C2A" stroke="#000" stroke-miterlimit="10" width="413" height="413"></rect><path fill="#CC2249" d="M0,250C0,131,0,84,42,42S130,0,250,0s166,0,208,42s42,89,42,208s0,166-42,208s-88,42-208,42s-166,0-208-42S0,369,0,250z M455,250c0-114,0-148-29-176c-29-29-62-29-176-29S102,45,74,74c-29,29-29,62-29,176s0,148,29,176c29,29,62,29,176,29s148,0,176-29 C455,397,455,364,455,250z M250,122c70.7000122,0,128,57.3000031,128,128s-57.2999878,128-128,128s-128-57.2999878-128-128 S179.3000031,122,250,122z M250,333c46,0,83-37,83-83s-37-83-83-83s-83,37-83,83S204,333,250,333z M413,117c0,16-13,30-30,30 c-16,0-30-14-30-30c0-17,14-30,30-30C400,87,413,100,413,117z"></path><image alt="Instagram" height="16" src="/rayschwartz/hp-responsive/assets/images/icons/icon-16-instagram.png" width="16" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href=""></image></svg></a></li>
+            </ul>
+        </div>
+        <div class="u-ExtendedFooter__listGroup">
+            <h3 class="u-ExtendedFooter__listHeader">Log In</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Your Account</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Alumnae/i Directory</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Class Notes</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Classifieds</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Alumnae/i Email</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Request a Transcript</a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Calendars</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Reunion</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Alumnae/i Events</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">On Campus<br>
+            </a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Contact</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Staff Directory</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Visit</a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Community</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Alumnae/i Association</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Classes</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Clubs</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Affiliate Groups</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Athletics</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Vassar Postcards</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Social Media</a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Publications</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Vassar Stories</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Vassar Quarterly</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Enews Digest</a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Services</h3>
+            <ul class="u-ExtendedFooter__list u-ExtendedFooter__list--services">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Career Center</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Travel</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Alumae House</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Alum Benefits</a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Support</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Want to Volunteer?</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Current Volunteers</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Send a Broadcast Email</a></li>
+            </ul>
+            <h3 class="u-ExtendedFooter__listHeader">Give</h3>
+            <ul class="u-ExtendedFooter__list">
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Ways to Give</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">The Vassar Fund</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Brewers Fund</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Gift Planning</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Donor Societies</a></li>
+            <li><a href="" class="u-ExtendedFooter__listItemLink">Questions about Giving</a></li>
+            </ul>
+        </div>
+    </div>
+CONTENT
+);
